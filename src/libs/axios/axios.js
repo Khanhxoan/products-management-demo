cd4f23ec7a0038f54d6a9abef43547dab34a80b9
khanhxoan <khanhxoan99@gmail.com>
2024-10-05 17:24:49 +0700

    feat: handle fail request api

--- src/libs/axios/axios.js
+++ src/libs/axios/axios.js
@@ -1,8 +1,10 @@
-import axios from 'axios';
-import qs from 'qs';
+import axios from "axios";
+import qs from "qs";
 
-import { logout } from '@/apis/auth';
-import { clearUserLogout } from '@/stores/authSlice/authSlice';
+import { logout } from "@/apis/auth";
+import { notify } from "@/components/custom-toast/custom-toast";
+import { TOAST_STATUS } from "@/constants/contants";
+import { clearUserLogout } from "@/stores/authSlice/authSlice";
 
 const baseURL = "https://product-management-server-expressjs.vercel.app";
 
@@ -32,9 +34,28 @@ defaultAxios.interceptors.response.use(
         return response;
     },
     (error) => {
-        const { response } = error;
-        if (response && response.status === 401) {
-            handleLogout();
+        const { response, request, message } = error;
+
+        if (response && response.status) {
+            switch (response.status) {
+                case 400:
+                    notify("Bad Request! Please check the input data.", TOAST_STATUS.ERORR);
+                    break;
+                case 401:
+                    handleLogout();
+                    notify("Unauthorized! Please log in again.", TOAST_STATUS.ERORR);
+                    break;
+                case 500:
+                    notify("Internal Server Error! Please try again later.", TOAST_STATUS.ERORR);
+                    break;
+                default:
+                    notify("An unexpected error occurred!", TOAST_STATUS.ERORR);
+                    break;
+            }
+        } else if (request) {
+            notify("No response from server. Please check your internet connection.", TOAST_STATUS.ERORR);
+        } else {
+            notify("Request failed: " + message ?? "", TOAST_STATUS.ERORR);
         }
         return Promise.reject(error);
     }
